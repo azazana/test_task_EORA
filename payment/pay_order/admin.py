aea850d5144ecb82fb0ba3e9b122e1a2471d8ef7
azazana <aazazana@gmail.com>
2022-11-24 17:45:47 +0300

    Migrate

--- payment/pay_order/admin.py
+++ payment/pay_order/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Item, Order, OrderItem, Discount, Tax
+from .models import Item, Order, OrderItem, Discount
 
 admin.site.register(Item)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Discount)
-admin.site.register(Tax)
+
--- payment/pay_order/models.py
+++ payment/pay_order/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
 
-class Tax(models.Model):
-    percent = models.IntegerField(default=0)
-
-    def __str__(self):
-        return str(self.percent)
-
-
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     price = models.FloatField(default=0)
-    tax = models.ForeignKey(Tax, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
